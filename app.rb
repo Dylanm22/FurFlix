bcf56aa6044b462123f0a662ccc8d2d4e0858b7d
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-01-14 05:45:59 +0000

    mini pro

--- app.rb
+++ app.rb
@@ -1,9 +1,43 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+
+#making a web app that ask for animal name then app puts out a picture of said animal and a random name for animal
+
+
+
+
+
+
 
 get("/") do
+  
+  
+  
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+
+erb(:home)
+
+end
+
+
+get("/:animal") do
+
+  url = "https://random.dog/woof.json"
+  raw = HTTP.get(url)
+  
+ 
+  raw_string = raw.to_s
+
+parsed_string = JSON.parse(raw_string)
+  
+@animal_pic = parsed_string.fetch("url")
+ 
+  pp @animal
+  erb(:animal_page)
+
+
 end
