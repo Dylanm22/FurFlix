940c886cdda38112f785b64b878d78d7dd2f051b
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-01-30 14:24:33 +0000

    minipj1

--- app.rb
+++ app.rb
@@ -5,119 +5,91 @@ require "http"
 #making a web app that ask for animal name then app puts out a picture of said animal and a random name for animal
 
 get("/") do
-  
- 
-  
-
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
 
-erb(:home)
-
-
-
+  erb(:home)
 end
-
 
 get("/dog") do
-
   url = "https://random.dog/woof.json"
   raw = HTTP.get(url)
-  
- 
+
   raw_string = raw.to_s
 
-parsed_string = JSON.parse(raw_string)
-  
-@animal_pic = parsed_string.fetch("url")
- 
+  parsed_string = JSON.parse(raw_string)
+
+  @animal_pic = parsed_string.fetch("url")
+
   pp @animal
   erb(:dog)
-
-
-end
-  
-
-get("/cat")do 
-
-url = "https://api.thecatapi.com/v1/images/search?limit=1"
-raw_data = HTTP.get(url)
-parsed_string = JSON.parse(raw_data)
-
-cat_aray = parsed_string[0]
-
-@cat_pic = cat_aray.fetch("url")
-
-erb(:cat)
-
-end 
-
-get("/duck")do
-url = "https://random-d.uk/api/v2/random"
-raw_data = HTTP.get(url)
-parsed_string = JSON.parse(raw_data)
-@duck_pic = parsed_string.fetch("url")
-
-erb(:duck)
-
-
-end 
-
-
-get("/fox")do 
-
-url="https://randomfox.ca/floof/"
-
-raw_data = HTTP.get(url)
-parsed_string = JSON.parse(raw_data)
-@fox_pic= parsed_string.fetch("image")
-
-erb(:fox)
-
 end
 
+get("/cat") do
+  url = "https://api.thecatapi.com/v1/images/search?limit=1"
+  raw_data = HTTP.get(url)
+  parsed_string = JSON.parse(raw_data)
 
-get("/random")do 
+  cat_aray = parsed_string[0]
 
-url = "https://random.dog/woof.json"
+  @cat_pic = cat_aray.fetch("url")
+
+  erb(:cat)
+end
+
+get("/duck") do
+  url = "https://random-d.uk/api/v2/random"
+  raw_data = HTTP.get(url)
+  parsed_string = JSON.parse(raw_data)
+  @duck_pic = parsed_string.fetch("url")
+
+  erb(:duck)
+end
+
+get("/fox") do
+  url = "https://randomfox.ca/floof/"
+
+  raw_data = HTTP.get(url)
+  parsed_string = JSON.parse(raw_data)
+  @fox_pic = parsed_string.fetch("image")
+
+  erb(:fox)
+end
+
+get("/random") do
+  url = "https://random.dog/woof.json"
   raw = HTTP.get(url)
-  
- 
+
   raw_string = raw.to_s
 
-parsed_string = JSON.parse(raw_string)
-  
-@animal_pic = parsed_string.fetch("url")
- 
- 
-url = "https://api.thecatapi.com/v1/images/search?limit=1"
-raw_data = HTTP.get(url)
-parsed_string = JSON.parse(raw_data)
+  parsed_string = JSON.parse(raw_string)
 
-cat_aray = parsed_string[0]
+  @animal_pic = parsed_string.fetch("url")
 
-@cat_pic = cat_aray.fetch("url")
+  url = "https://api.thecatapi.com/v1/images/search?limit=1"
+  raw_data = HTTP.get(url)
+  parsed_string = JSON.parse(raw_data)
 
+  cat_aray = parsed_string[0]
 
-url = "https://random-d.uk/api/v2/random"
-raw_data = HTTP.get(url)
-parsed_string = JSON.parse(raw_data)
-@duck_pic = parsed_string.fetch("url")
+  @cat_pic = cat_aray.fetch("url")
 
-url="https://randomfox.ca/floof/"
+  url = "https://random-d.uk/api/v2/random"
+  raw_data = HTTP.get(url)
+  parsed_string = JSON.parse(raw_data)
+  @duck_pic = parsed_string.fetch("url")
 
-raw_data = HTTP.get(url)
-parsed_string = JSON.parse(raw_data)
-@fox_pic= parsed_string.fetch("image")
+  url = "https://randomfox.ca/floof/"
 
+  raw_data = HTTP.get(url)
+  parsed_string = JSON.parse(raw_data)
+  @fox_pic = parsed_string.fetch("image")
 
+  random_array = [@cat_pic, @fox_pic, @animal_pic, @duck_pic]
 
-random_array=[@cat_pic,@fox_pic,@animal_pic,@duck_pic]
+  @random_pic = random_array.sample
 
-@random_pic=random_array.sample
-
-
-erb(:random)
+  erb(:random)
 end
